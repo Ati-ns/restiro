df59941e096c9758126e32ec5569ba0aeb7bd83c
Ati-ns <Ati-ns@gitlab.com>
2019-02-03 12:14:22 +0330

    Remove previous test project

--- webtest_docgen/tests/stuff/test_project/test_api_define.py
+++
@@ -1,8 +0,0 @@
-"""
-    @apiDefine CommissionReportSuccess for test commission
-                                        and some others
-    @apiSuccess {String} id
-    @apiSuccess {String} userAccountId
-    @apiSuccess {String} commissionReport file address
-    @apiSuccess {DateTime} createdAt
-"""
